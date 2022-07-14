0416eca39ad25767d2bf25cbb1d519a1078592a5
Paulo Borges <borgespaulo72@yahoo.com.br>
2022-07-14 00:06:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var typed = new Typed
+(".typing", {
+
+    strings:["Desenvolvedor", "Freelancer", "Programador"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+  })
+
+
+var typed = new Typed
+  (".typing-2", {
+
+    strings:["RocketLinks", "FrontPortolio", "layng", "RocketCoffee", "RocketPortfolio"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+  })
+
+  var typed = new Typed
+  (".typing-3", {
+
+    strings:["HTML", "CSS", "JAVASCRIPT"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+  })
+
+
+
